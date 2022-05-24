ad032283f4186ef6898cf7b03e6e5e52e70f131a
Mikhail Verkhorubov <verhorubov.mihail@gmail.com>
2022-05-24 17:43:46 +0700

    modified

--- 9_NeedLOptimization.py
+++ 9_NeedLOptimization.py
@@ -6,13 +6,19 @@
 #c = 3
 #n = 1
 #a*a + b*b = c*c
+import time
+
+start_time = time.time()
+
 flag = 0
 for a in range(1, 1000): #Найти оптимальный способ поиска
     if flag == 1:
         break
     for b in range(a, 1000):
         for c in range(b, 1000):
-            if a + b + c == 1000 and a*a + b*b - c*c == 0 and a < b < c:
+            if a + b + c == 1000 and a*a + b*b - c*c == 0:
             #if 2*(-a*b+2000*(a+b-500)) and a + b + c == 1000 and a < b < c:
                 print(a*b*c)
                 flag = 1
+
+print("--- %s seconds ---" % (time.time() - start_time))
